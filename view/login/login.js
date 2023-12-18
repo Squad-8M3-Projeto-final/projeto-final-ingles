083b56089964f70d23bdca8b744a09d9423d69ae
Daniel <116726675+zedaesquina1@users.noreply.github.com>
2023-12-17 21:12:55 -0300

    agora sim

--- view/login/login.js
+++
@@ -1,8 +0,0 @@
-function doLogin() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    // Aqui você pode adicionar a lógica de autenticação, por exemplo, verificar em um banco de dados
-    // Neste exemplo, vamos apenas exibir uma mensagem de sucesso no console.
-    console.log('Login successful! Welcome, ' + username);
-  }
